7638336e4aacaaf5715c45c152f4238d226bc7be
daniel <24758309+unanoc@users.noreply.github.com>
2022-09-05 14:52:22 +0900

    Add fields for types.Tx

--- primitives/types/tx.go
+++ primitives/types/tx.go
@@ -11,7 +11,11 @@ const (
 	StatusCompleted Status = "completed"
 	StatusError     Status = "error"
 
-	TxTransfer TransactionType = "transfer"
+	TxTransfer   TransactionType = "transfer"
+	TxDelegate   TransactionType = "delegate"
+	TxRedelegate TransactionType = "redelegate"
+	TxUnbound    TransactionType = "unbound"
+	TxReward     TransactionType = "reward"
 )
 
 type (
@@ -21,31 +25,63 @@ type (
 	}
 
 	Tx struct {
-		// Unique identifier
-		ID string `json:"id" bson:"id"`
-		// Address of the transaction sender
-		From string `json:"from" bson:"from"`
-		// Address of the transaction recipient
-		To string `json:"to" bson:"to"`
-		// Unix timestamp of the block the transaction was included in
-		BlockCreatedAt int64 `json:"block_created_at" bson:"block_created_at"`
-		// Height of the block the transaction was included in
-		Block uint64 `json:"block_num" bson:"block_num"`
-		// Status of the transaction e.g: "completed", "pending", "error"
-		Status Status `json:"status" bson:"status"`
-		// Empty if the transaction "completed" or "pending", else error explaining why the transaction failed (optional)
-		Error string `json:"error,omitempty" bson:"error"`
-		// Type of metadata
-		Type TransactionType `json:"type" bson:"type"`
-		// Metadata data object
-		Metadata interface{} `json:"metadata" bson:"metadata"`
+		ID             string          `json:"id" bson:"id"`
+		From           string          `json:"from" bson:"from"`
+		To             string          `json:"to" bson:"to"`
+		BlockCreatedAt int64           `json:"block_created_at" bson:"block_created_at"`
+		Block          uint64          `json:"block_num" bson:"block_num"`
+		Status         Status          `json:"status" bson:"status"`
+		Error          string          `json:"error,omitempty" bson:"error"`
+		Type           TransactionType `json:"type" bson:"type"`
+		Metadata       interface{}     `json:"metadata" bson:"metadata"`
+		GasUsed        string          `json:"gas_used" bson:"gas_used"`
+		GasWanted      string          `json:"gas_wanted" bson:"gas_wanted"`
+		Fee            Fee             `json:"fee"`
+	}
+
+	Fee struct {
+		Asset string `json:"asset"`
+		Value Amount `json:"value"`
 	}
 
 	Txs []Tx
 
-	// Transfer describes the transfer of currency
+	// Transfer describes the transfer metadata
 	Transfer struct {
-		Asset string `json:"asset"`
-		Value Amount `json:"value"`
+		FromAddr string `json:"from_address"`
+		ToAddr   string `json:"to_address"`
+		Asset    string `json:"asset"`
+		Value    Amount `json:"value"`
+	}
+
+	// Delegate describes the delegate metadata
+	Delegate struct {
+		DelegatorAddr string `json:"delegator_address"`
+		ValidatorAddr string `json:"validator_address"`
+		Asset         string `json:"asset"`
+		Value         Amount `json:"value"`
+	}
+
+	// Redelegate describes the redelegate metadata
+	Redelegate struct {
+		DelegatorAddr    string `json:"delegator_address"`
+		ValidatorSrcAddr string `json:"validator_src_address"`
+		ValidatorDstAddr string `json:"validator_dst_address"`
+		Asset            string `json:"asset"`
+		Value            Amount `json:"value"`
+	}
+
+	// Unbound describes the unbound metadata
+	Unbound struct {
+		DelegatorAddr string `json:"delegator_address"`
+		ValidatorAddr string `json:"validator_address"`
+		Asset         string `json:"asset"`
+		Value         Amount `json:"value"`
+	}
+
+	// Reward describes the reward metadata
+	Reward struct {
+		DelegatorAddr string `json:"delegator_address"`
+		ValidatorAddr string `json:"validator_address"`
 	}
 )
